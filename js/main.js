1bf83c4e60724ce1e3d8e67fff6897a70825560f
adalie <adalieschroter@gmail.com>
2022-12-08 19:17:25 +0100

    Changed email alert to error message

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
+var error = document.getElementById("error");
+
+
 function checkEmail() {
     var email = document.getElementById('email');
     var filter = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (!filter.test(email.value)) {
-        alert('Please provide a valid email address');
+        //alert('Please provide a valid email address');
+        document.addEventListener("click", function(event){
+            event.preventDefault()
+        });
+        error.textContent = "Please enter a valid email";
+        error.style.color = "black";
         email.focus;
         return false;
     }
